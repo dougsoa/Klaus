fbfd7d270d3dc7fe56a1ad3d247350184746acfa
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2023-04-20 13:35:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,44 @@ function activeScroll(){
 }
 
 window.addEventListener('scroll', activeScroll);
+
+// fazer o botão Up ir ao topo 
+const btn = document.getElementById("top")
+
+btn.addEventListener("click", function() {
+  window.scrollTo(0,0)
+})
+
+// função para o botão Up desaparecer no topo
+document.addEventListener('scroll', ocultar)
+
+function ocultar() {
+  if(window.scrollY > 250) {
+    btn.style.display = "flex"
+  } else {
+    btn.style.display = "none"
+  }
+}
+ ocultar()
+
+
+// FUNÇÃO PARA QUE O ONCLICK ADD AOS BOTÕES DO MENU DIRECIONEM PARA SEUS RESPECTIVOS CAMINHOS
+function index() {
+    window.location = "index.html" ;
+  }
+  
+  function projetos() {
+    window.location = "projetos.html" ;
+  }
+  
+  function galeria() {
+    window.location = "galeria.html" ;
+  }
+    
+  function loja() {
+    window.location = "loja.html" ;
+  }
+    
+  function contato() {
+    window.location = "contato.html" ;
+  }
